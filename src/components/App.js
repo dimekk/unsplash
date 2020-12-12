095947ad850451f2e134503d81de38af4b16b391
Wojciech Burczyk <wojciech.burczyk@hotelgram.pl>
2020-12-12 15:08:11 +0100

    fixing app build

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../App.css';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className="App">
+                <h1>henlo</h1>
+            </div>
+        )
+    }
+}
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,34 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    entry: {
-      index: './src/index.js'
-    },
-    output: {
-      path: __dirname + '/build',
-      filename: 'index.bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js|\.jsx$/,
-          exclude: /node_modules/,
-          use: 'babel-loader'
-        }
-      ]
-    },
-    plugins: [
-      new HTMLWebpackPlugin({
-        template: './src/index.html'
-      })
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
     ]
-  }
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "index.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
