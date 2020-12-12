230166aa24bb488e5eac1a0e32ea40ebdbf62af9
Wojciech Burczyk <wojciech.burczyk@hotelgram.pl>
2020-12-12 13:53:50 +0100

    setting up package.json

---
+++ src/comps/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			<h1>henlo</h1>
+		</div>
+	)
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: {
+      index: './src/index.js'
+    },
+    output: {
+      path: __dirname + '/build',
+      filename: 'index.bundle.js'
+    }
+  }
